59814fd471af04dda5405bbcd85858f0c0aff68a
JD <jann.driessen@gmx.de>
2025-07-04 22:46:25 +0200

    add privy (provider)

---
+++ app/app/providers.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { PrivyProvider } from '@privy-io/react-auth'
+
+console.log(process.env.NEXT_PUBLIC_PRIVY_APP_ID)
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: 'users-without-wallets',
+          },
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  )
+}

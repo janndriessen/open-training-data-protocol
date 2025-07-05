8322e3e3ba9d4b3cd34ee011d8b07737f2d33618
JD <jann.driessen@gmx.de>
2025-07-05 10:48:30 +0200

    add logout

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -1,8 +1,12 @@
+'use client'
+
+import { usePrivy } from '@privy-io/react-auth'
+
 import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
-import { SidebarTrigger } from '@/components/ui/sidebar'
 
 export function SiteHeader() {
+  const { ready, authenticated, logout } = usePrivy()
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -23,6 +27,26 @@ export function SiteHeader() {
               GitHub
             </a>
           </Button>
+          {ready && authenticated && (
+            <Button
+              variant="ghost"
+              asChild
+              size="sm"
+              className="hidden sm:flex"
+            >
+              <button
+                onClick={async () => {
+                  const result = logout()
+                  if (result && typeof result.then === 'function') {
+                    await result
+                  }
+                  window.location.href = '/'
+                }}
+              >
+                Logout
+              </button>
+            </Button>
+          )}
         </div>
       </div>
     </header>
